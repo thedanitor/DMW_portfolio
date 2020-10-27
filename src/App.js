c40a8540cdc52ac52fac09c6f393b0866a0e4bb2
Dan Weikart <danmweikart@gmail.com>
2020-10-27 15:16:18 -0700

    Added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 // import './App.css';
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
@@ -8,6 +13,10 @@ export default function App() {
       <Navbar>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/skills" component={Skills} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
       </Navbar>
     </Router>
