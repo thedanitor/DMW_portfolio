18602096efcec17895199f9b028a26367423c6a1
Dan Weikart <danmweikart@gmail.com>
2020-11-12 13:08:05 -0800

    Edited About

--- src/pages/About.js
+++ src/pages/About.js
@@ -38,11 +38,11 @@ export default function About() {
           <Typography variant="p">
             Recent graduate of University of Washington's coding bootcamp for
             full-stack web development. I have learned so much since I started
-            the course and I am improving and learning more everyday.
+            the course and I am improving and learning more daily.
           </Typography>
           <Typography variant="p">
             My hobbies include hiking, camping, biking, reading, cooking,
-            listening to, and playing music. Causes I am dedicated to are social
+            listening to and playing music. Causes I am dedicated to are social
             justice, sustainability, and climate change. It would be my dream to
             incorporate any of these passions into my work.
           </Typography>
