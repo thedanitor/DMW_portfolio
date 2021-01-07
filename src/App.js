156ed586329420c384083e5e1cd6b5f9a6199f43
Dan Weikart <danmweikart@gmail.com>
2021-01-06 18:00:00 -0800

    updated router

--- src/App.js
+++ src/App.js
@@ -26,11 +26,11 @@ export default function App() {
   const classes = useStyles();
   return (
     <div className={classes.backgroundApp}>
-   <Router>
+   <Router basename="/DMW_portfolio">
       <ThemeProvider theme={createMuiTheme}>
       <Appbar />
         <Switch>
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/skills" component={Skills} />
